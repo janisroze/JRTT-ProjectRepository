5d45e92e42a62f1905277c9fb0fc451e99348c11
janisroze <janis.roze1@gmail.com>
2012-10-15 15:43:15 +0300

    initial commit

---
+++ www/jquery/main.js
@@ -0,0 +1,24 @@
+ document.addEventListener("deviceready", onDeviceReady, false);
++      function onDeviceReady() {
++        db = window.sqlitePlugin.openDatabase("Database", "1.0", "PhoneGap Demo", 200000);
++
++        db.transaction(function(tx) {
++          tx.executeSql('DROP TABLE IF EXISTS test_table');
++          tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
++
++          tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
++          console.log("insertId: " + res.insertId + " -- probably 1"); // check #18/#38 is fixed
++          alert("insertId: " + res.insertId + " -- should be valid");
++
++            db.transaction(function(tx) {
++              tx.executeSql("SELECT data_num from test_table;", [], function(tx, res) {
++                console.log("res.rows.length: " + res.rows.length + " -- should be 1");
++                alert("res.rows.item(0).data_num: " + res.rows.item(0).data_num + " -- should be 100");
++              });
++            });
++
++          }, function(e) {
++            console.log("ERROR: " + e.message);
++          });
++        });
++      }
